73f82d01a0b478cd3f7729665515bb126a428c95
noelfoka <nomgnenoel@gmail.com>
2024-06-01 17:29:06 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import toast from 'react-hot-toast';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count, setCount] = useState(0);
+  const [stoped, setStoped] = useState(false);
+
+  const increment = () => {
+    const newCount = count + 1
+    setCount(newCount)
+    if (newCount % 3) {
+      toast.success("Count est 3!")
+      setCount(newCount * 5)
+      setStoped(true)
+    } else {
+      setCount(newCount)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ToastContainer />
+      <h1>test des state</h1>
+      <button type='button' onClick={increment}>{count}</button>
     </>
   )
 }
